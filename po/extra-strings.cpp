b72e2e98736e168c58c40fb3c988c83b06211ffb
Remi Peyronnet <remi+github@via.ecp.fr>
2022-11-22 21:47:02 +0100

    strings for #5 and #6

--- po/extra-strings.cpp
+++ po/extra-strings.cpp
@@ -4,7 +4,12 @@ wxString _("About %s");
 wxString _("Translators");
 
 // Strings for config.xml default items
+wxString _("Display only added");
+wxString _("Display only removed");
+wxString _("Display only added or removed");
 wxString _("Display only added, removed or equal");
+
+wxString _("Display HTML Lists of differences");
 
 wxString _("All Nodes");
 wxString _("All Nodes named nodeName");
@@ -13,3 +18,4 @@ wxString _("All Nodes with attribute named attributeName");
 wxString _("All Nodes with attribute named attributeName with value toto");
 wxString _("All Nodes with content text toto");
 wxString _("Added Nodes");
+
